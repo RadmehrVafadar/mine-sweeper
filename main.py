9548f24434e9f5d76251a067db49b4a432b19f10
Radmehr <23xf1@queensu.ca>
2025-08-17 11:43:51 -0400

    Inroduce land and flag sprites

--- main.py
+++ main.py
@@ -2,18 +2,30 @@
 import pygame
 from my_module import Button
 
+
 # pygame setup
+window_width = 700
+window_height = 700
+
 pygame.font.init()
+pygame.display.set_caption("Mines")
 pygame.init()
-screen = pygame.display.set_mode((700, 700))
+screen = pygame.display.set_mode((window_width, window_height))
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
+# Load button images
+blank_land = pygame.image.load('./source/Land.png').convert_alpha()
+flag_land = pygame.image.load('./source/land_with_flag.png').convert_alpha()
+
+# mine board setup
 board_width = 16
 board_height = 16
+mine_matrix = [[Button(40*i,40*j, 32,32, "") for j in range(board_width)] for i in range(board_height)]
 
-mine_matrix = [[Button(40*i,40*j, 30,30, "") for j in range(board_width)] for i in range(board_height)]
+
+
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 while running:
@@ -25,6 +37,11 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("gray")
+
+
+
+
+    # display the button elements in the screen
     for row in mine_matrix:
         for mine in row:
             mine.draw(screen)
